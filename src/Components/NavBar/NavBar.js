e2e14192e3a1e7c616cd1beaccaec66e4abbe14d
a-heredia <alvalemas@gmail.com>
2019-12-07 13:35:04 +0100

    Change to nalinks

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 
 import Home from '../Home/Home';
 import Albums from '../Albums/Albums';
 import About from '../About/About';
+
+import './NavBar.css'
 
 class NavBar extends Component {
 
@@ -11,14 +13,14 @@ class NavBar extends Component {
     return (
       <Router>
         <div className="NavBar">
-          <div className="link-container">
-            <Link to="/" className="link">Home</Link>
+          <div className="navbar-container">
+            <NavLink to="/" exact activeClassName="active" className="navbar-link">Home</NavLink>
           </div>
-          <div className="link-container">
-            <Link to="/albumes" className="link">Albumes</Link>
+          <div className="navbar-container">
+            <NavLink to="/albumes" activeClassName="active" className="navbar-link">Albumes</NavLink>
           </div>
-          <div className="link-container">
-            <Link to="/about" className="link">About</Link>
+          <div className="navbar-container">
+            <NavLink to="/about" activeClassName="active" className="navbar-link">About</NavLink>
           </div>
           <Route path="/" exact component={Home} />
           <Route path="/albumes" component={Albums} />
